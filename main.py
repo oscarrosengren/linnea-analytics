fc96c98079ff8f0c9de40d27c14952704917e928
oscarrosengren <80699317+oscarrosengren@users.noreply.github.com>
2025-03-11 20:15:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ import subprocess
 
 # Streamlit UI
 st.title("Linnea Script")
-
-#repo_dir = os.path.dirname(os.path.abspath(__file__))
+ 
+ repo_dir = os.path.dirname(os.path.abspath(__file__))
+ scripts = get_python_scripts(repo_dir)
